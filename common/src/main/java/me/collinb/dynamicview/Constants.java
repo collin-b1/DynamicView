8870b084ce5bc70d5116ebf360d14d0a392cc20d
collin-b1 <github.1wjd0@simplelogin.com>
2026-02-21 20:48:37 -0500

    Initial commit

---
+++ common/src/main/java/me/collinb/dynamicview/Constants.java
@@ -0,0 +1,11 @@
+package me.collinb.dynamicview;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "dynamicview";
+    public static final String MOD_NAME = "DynamicView";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
